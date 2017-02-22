478ba032221801935d8cfc611ec686484628ad98
nathan-siu <nathansiu9730@gmail.com>
2017-02-22 10:36:59 -0500

    ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {AngularFireModule} from 'angularfire2';
+
+export const firebaseConfig = {
+   apiKey: "AIzaSyDf6wrehtgP8QKcijvy4daPidjyR6ZpB6E",
+    authDomain: "midterm-f1c55.firebaseapp.com",
+    databaseURL: "https://midterm-f1c55.firebaseio.com",
+    storageBucket: "midterm-f1c55.appspot.com",
+    messagingSenderId: "791583500981"
+};
 
 @NgModule({
   declarations: [
@@ -15,7 +24,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+/*
+File Name: home.ts
+Student num: 200281793  
+Authors Name: Nathan Siu
+*/
 
-import { NavController } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController,NavParams } from 'ionic-angular';
+//import angular fire 2
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+shopLists: FirebaseListObservable<any>;// reference to the database
 
-  constructor(public navCtrl: NavController) {
+public list:any;
 
+  constructor(public navCtrl: NavController, public navParams:NavParams, af: AngularFire) {
+     
+    this.shopLists = af.database.list('/shoppingList');//get the list from db
+     this.list={};//list object 
+  
+    
+  }
+
+  //methods
+
+//save list to db
+  saveList(){
+    console.log(this.list); //log to console
+    this.shopLists.push(this.list);// saves list to db
+    this.navCtrl.push(HomePage);// redirect to home page
+
+  }
+  cancelButton(){
+    this.navCtrl.push(HomePage);// redirect to home page
   }
 
 }
